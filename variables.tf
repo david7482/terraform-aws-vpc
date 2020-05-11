6b03ab3c74fc75a87641371678316bf1a0279130
david7482 <david74.chou@gmail.com>
2020-05-12 01:27:31 +0800

    feat(tf): implement AWS VPC provision
    * Create a VPC with the specified CIDR.
    * Create both public and private subnets in the specified AZ(s) with the designed CIDR.
    * Create internet gateway and NAT gateway
    * Create S3 VPC endpoint so we could access S3 directly within VPC.

---
+++ variables.tf
@@ -0,0 +1,26 @@
+variable "region" {
+  default = ""
+}
+
+variable "name" {
+  default = ""
+}
+
+variable "tags" {
+  type    = map(string)
+  default = {}
+}
+
+variable "cidr" {
+  default = "0.0.0.0/0"
+}
+
+variable "az_subnets" {
+  type = list(object({
+    az           = string
+    public_cidr  = string
+    private_cidr = string
+  }))
+
+  default = []
+}
